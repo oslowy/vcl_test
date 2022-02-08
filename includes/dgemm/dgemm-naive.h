6ddcee4626546b9315082c031a0293a77aaf08ef
Orion Lowy <oslowy@uh.edu>
2022-02-08 12:49:03 -0600

    implemented dgemm_naive from hw1

---
+++ includes/dgemm/dgemm-naive.h
@@ -0,0 +1,29 @@
+//
+// Created by Orion on 2/8/2022.
+//
+
+#ifndef VCL_TEST_DGEMM_NAIVE_H
+#define VCL_TEST_DGEMM_NAIVE_H
+
+const char* dgemm_desc = "Naive, three-loop dgemm.";
+
+/* This routine performs a dgemm operation
+ *  C := C + A * B
+ * where A, B, and C are lda-by-lda matrices stored in column-major format.
+ * On exit, A and B maintain their input values. */
+void square_dgemm (int n, double* A, double* B, double* C)
+{
+    /* For each row i of A */
+    for (int i = 0; i < n; ++i)
+        /* For each column j of B */
+        for (int j = 0; j < n; ++j)
+        {
+            /* Compute C(i,j) */
+            double cij = C[i+j*n];
+            for( int k = 0; k < n; k++ )
+                cij += A[i+k*n] * B[k+j*n];
+            C[i+j*n] = cij;
+        }
+}
+
+#endif //VCL_TEST_DGEMM_NAIVE_H
