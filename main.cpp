2acb71e87d43df8f4900568f6d4b8270a70d7d70
Orion Lowy <oslowy@uh.edu>
2022-02-08 12:03:33 -0600

    vcl test example 2.1 implemented

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+// Simple vector class example C ++ file
+#include <cstdio>
+#include "vectorclass.h"
+int main () {
+// define and initialize integer vectors a and b
+    Vec4i a (10,11,12,13);
+    Vec4i b (20,21,22,23);
+// add the two vectors
+    Vec4i c = a + b;
+// Print the results
+    for (int i = 0; i < c.size(); i++) {
+        printf ("%5i", c[i]);
+    }
+    printf ("\n");
     return 0;
 }
