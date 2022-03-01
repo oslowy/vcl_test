53da659390510e5c3342ce0ba0da6a5af79decfc
Orion Lowy <oslowy@uh.edu>
2022-03-01 17:06:51 -0600

    Non-blocked four-accumulator versions

--- DgemmNonBlocked4AccJKI.cpp
+++ DgemmNonBlocked4AccJKI.cpp
@@ -7,29 +7,38 @@
 void DgemmNonBlocked4AccJKI::square_dgemm(int n, const double *A, const double *B, double *C) {
     const int FOUR_ACC_LIMIT = n - 3; //Stop accumulating with four accumulators early so does not go out of bounds
 
+    /* Compute the partial sum for four accumulators */
     for (int j = 0; j < n; ++j)
     {
         int k;
         for (k = 0; k < FOUR_ACC_LIMIT; ++k)
         {
             /* Pre-store B(k, j) to avoid repeated memory access */
+            double bk0j = B[k+j*n];
+            double bk1j = B[k+1+j*n];
+            double bk2j = B[k+2+j*n];
+            double bk3j = B[k+3+j*n];
+
+            /* Compute next partial sum on C(i,j) */
+            for( int i = 0; i < n; i++ )
+            {
+                C[i+j*n] += A[i+(k)*n] * bk0j;
+                C[i+j*n] += A[i+(k+1)*n] * bk1j;
+                C[i+j*n] += A[i+(k+2)*n] * bk2j;
+                C[i+j*n] += A[i+(k+3)*n] * bk3j;
+            }
+        }
+
+        /* Finish remaining elements not covered by the four accumulators using one accumulator */
+        for(; k < n; ++k)
+        {
             double bkj = B[k+j*n];
 
             /* Compute next partial sum on C(i,j) */
             for( int i = 0; i < n; i++ )
                 C[i+j*n] += A[i+k*n] * bkj;
         }
-
-        for(; k < n; ++k)
-        {
-            double bkj = A[k+j*n];
-
-            /* Compute next partial sum on C(i,j) */
-            for( int i = 0; i < n; i++ )
-                C[i+j*n] += A[i+k*n] * bkj;
-        }
     }
-
 }
 
 const char *DgemmNonBlocked4AccJKI::dgemm_desc() {
